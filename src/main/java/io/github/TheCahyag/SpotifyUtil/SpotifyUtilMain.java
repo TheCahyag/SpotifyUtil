5a1f53d9f12596e98bdd772e28ce5b01ef14594c
Brandon Bires-Navel <brandonnavel@outlook.com>
2018-12-22 20:23:14 -0500

    Initial commit

---
+++ src/main/java/io/github/TheCahyag/SpotifyUtil/SpotifyUtilMain.java
@@ -0,0 +1,14 @@
+package io.github.TheCahyag.SpotifyUtil;
+
+import io.github.TheCahyag.SpotifyUtil.gui.SpotifyUtilGUI;
+
+/**
+ * File: SpotifyUtilMain.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class SpotifyUtilMain {
+    public static void main(String[] args) {
+        SpotifyUtilGUI.launch();
+    }
+}
---
+++ src/main/java/io/github/TheCahyag/SpotifyUtil/gui/SpotifyUtilGUI.java
@@ -0,0 +1,16 @@
+package io.github.TheCahyag.SpotifyUtil.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * File: SpotifyUtilGUI.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class SpotifyUtilGUI extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
