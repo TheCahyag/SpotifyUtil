4e98f1a27ef4d6c94bd873feeda23b58dc7b9821
Brandon Bires-Navel <brandonnavel@outlook.com>
2019-01-02 14:00:32 -0500

    Added classes to represent the side menu

---
+++ src/main/java/io/github/TheCahyag/SpotifyUtil/gui/sidemenu/SideMenuItem.java
@@ -0,0 +1,9 @@
+package io.github.TheCahyag.SpotifyUtil.gui.sidemenu;
+
+/**
+ * File: SideMenuItem.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class SideMenuItem {
+}
---
+++ src/main/java/io/github/TheCahyag/SpotifyUtil/gui/sidemenu/SideMenuPane.java
@@ -0,0 +1,9 @@
+package io.github.TheCahyag.SpotifyUtil.gui.sidemenu;
+
+/**
+ * File: SideMenuPane.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class SideMenuPane {
+}
