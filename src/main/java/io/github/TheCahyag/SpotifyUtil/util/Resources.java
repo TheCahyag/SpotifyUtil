2d68bace34ff22ce5e26a9748cfe2294d33e9247
Brandon Bires-Navel <brandonnavel@outlook.com>
2019-01-02 12:05:16 -0500

    App wide constants

---
+++ src/main/java/io/github/TheCahyag/SpotifyUtil/util/Resources.java
@@ -0,0 +1,12 @@
+package io.github.TheCahyag.SpotifyUtil.util;
+
+/**
+ * File: Resources.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class Resources {
+    public static Integer APP_HEIGHT = 500;
+    public static Integer APP_WIDTH = 500;
+    public static String APP_TITLE = "Spotify Util";
+}
