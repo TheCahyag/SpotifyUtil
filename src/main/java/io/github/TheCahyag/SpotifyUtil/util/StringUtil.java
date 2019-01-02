d4c572601ca01e301a9fb4ae6b96e03eaf3663e8
Brandon Bires-Navel <brandonnavel@outlook.com>
2019-01-02 12:49:35 -0500

    Added StringUtil class

---
+++ src/main/java/io/github/TheCahyag/SpotifyUtil/util/StringUtil.java
@@ -0,0 +1,13 @@
+package io.github.TheCahyag.SpotifyUtil.util;
+
+/**
+ * File: StringUtil.java
+ *
+ * @author Brandon Bires-Navel (brandonnavel@outlook.com)
+ */
+public class StringUtil {
+
+    public static boolean exists(String string){
+        return !(string == null || string.equals(""));
+    }
+}
