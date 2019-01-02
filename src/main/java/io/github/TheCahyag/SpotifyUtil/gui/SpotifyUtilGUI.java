6d0368107071171c34618afc23a1173c3bce98e9
Brandon Bires-Navel <brandonnavel@outlook.com>
2019-01-02 12:06:11 -0500

    Added GUI logic to display the playlists of a hard coded user

--- src/main/java/io/github/TheCahyag/SpotifyUtil/gui/SpotifyUtilGUI.java
+++ src/main/java/io/github/TheCahyag/SpotifyUtil/gui/SpotifyUtilGUI.java
@@ -1,7 +1,34 @@
 package io.github.TheCahyag.SpotifyUtil.gui;
 
+import com.wrapper.spotify.model_objects.specification.Playlist;
+import com.wrapper.spotify.model_objects.specification.PlaylistSimplified;
+import com.wrapper.spotify.model_objects.specification.Track;
+import com.wrapper.spotify.model_objects.specification.TrackSimplified;
+import io.github.TheCahyag.SpotifyUtil.SpotifyUtilMain;
+import io.github.TheCahyag.SpotifyUtil.data.PreparedStatements;
+import io.github.TheCahyag.SpotifyUtil.spotify.Connector;
+import io.github.TheCahyag.SpotifyUtil.spotify.SpotifyRequest;
+import io.github.TheCahyag.SpotifyUtil.gui.pane.TracksPane;
+import io.github.TheCahyag.SpotifyUtil.gui.pane.TracksSimplifiedPane;
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static io.github.TheCahyag.SpotifyUtil.util.Resources.*;
 
 /**
  * File: SpotifyUtilGUI.java
@@ -9,8 +36,95 @@ import javafx.stage.Stage;
  * @author Brandon Bires-Navel (brandonnavel@outlook.com)
  */
 public class SpotifyUtilGUI extends Application {
+
+    private Connector connector;
+
+    private Scene scene;
+    private BorderPane mainPane;
+    private VBox sideMenu;
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+
+        // Spotify api connection init
+        this.connector = Connector.getConnector();
+
+        // Frontend pane init
+        this.mainPane = new BorderPane();
+        this.sideMenu = new VBox(10);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        VBox vBox = new VBox(10);
+        vBox.getChildren().addAll(this.getSideMenuItems());
+        this.mainPane.setLeft(new ScrollPane(vBox));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(mainPane);
+
+        mainPane.setCenter(makeGridFromTracks(PreparedStatements.getAllTracks()));
+
+        Button btn = new Button("Play Brandy");
+        btn.setOnAction(event ->
+//                SpotifyRequest.playTrack(PreparedStatements.getTrack("2BY7ALEWdloFHgQZG6VMLA"))
+                        System.out.println(SpotifyRequest.getMe())
+        );
+
+
+        mainPane.setBottom(btn);
+
+
+        this.scene = new Scene(root, APP_WIDTH, APP_HEIGHT);
+        primaryStage.setTitle(APP_TITLE);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private List<Node> getSideMenuItems(){
+        List<Node> sideMenuItems = new ArrayList<>();
+
+        Text albums = new Text("Albums");
+        albums.setOnMouseClicked(event -> {
+            List<TrackSimplified> tracks = SpotifyRequest.getAlbumTracks();
+            this.mainPane.setCenter(makeGridFromTracksSimplified(tracks));
+        });
+
+        List<PlaylistSimplified> playlists = SpotifyRequest.getUserPlaylists(SpotifyUtilMain.thecahyagId);
+        playlists.forEach(playlistSimplified -> {
+            Text playlist = new Text(playlistSimplified.getName());
+            sideMenuItems.add(makeClickable(playlist, event ->  {
+                Playlist playlist1 = SpotifyRequest.getPlaylist(playlistSimplified.getId());
+                List<Track> trackSimplifieds = new ArrayList<>();
+                Arrays.stream(playlist1.getTracks().getItems()).forEach(playlistTrack -> trackSimplifieds.add(playlistTrack.getTrack()));
+                this.mainPane.setCenter(makeGridFromTracks(trackSimplifieds));
+            }));
+        });
+
+
+
+        sideMenuItems.add(albums);
+        return sideMenuItems;
+    }
+
+    private Node makeGridFromTracksSimplified(List<TrackSimplified> tracks){
+        return new ScrollPane(new TracksSimplifiedPane(tracks));
+    }
+
+    private Node makeGridFromTracks(List<Track> tracks){
+        return new ScrollPane(new TracksPane(tracks));
+    }
+
+    public Node makeClickable(Node node, EventHandler<? super MouseEvent> action){
+        node.setOnMouseClicked(action);
+        node.setOnMouseEntered(event -> scene.setCursor(Cursor.HAND));
+        node.setOnMouseExited(event -> scene.setCursor(Cursor.DEFAULT));
+        return node;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
